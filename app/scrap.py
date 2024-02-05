c76c96ed17a41effa03ee543638e04c5722f5ed0
romzy <romziqutbi@gmail.com>
2024-02-05 10:08:35 +0700

    init commit

---
+++ app/scrap.py
@@ -0,0 +1,5 @@
+def hello_world():
+    """
+    This function returns the string "Hello World!"
+    """
+    return "Hello World!"
---
+++ main.py
@@ -0,0 +1,3 @@
+from app.scrap import hello_world
+
+print(hello_world())
