1256cdb7b1d5dbdbbe50730c84a646959d55baec
romzy <romziqutbi@gmail.com>
2024-02-05 13:35:40 +0700

    base code

--- app/scrap.py
+++ app/scrap.py
@@ -1,5 +1,48 @@
-def hello_world():
+from bs4 import BeautifulSoup
+import requests
+import validators
+
+
+def get_soup(link: str):
+    if not validators.url(link):
+        return None
+    with requests.Session() as s:
+        res = s.get(link, timeout=5)
+    # print(res.content)
+    soup = BeautifulSoup(res.content, "html.parser")
+    # print(soup)
+
+    return soup
+
+
+def get_title(soup: BeautifulSoup, selector: str):
     """
-    This function returns the string "Hello World!"
+    Get the title from the BeautifulSoup object using the given selector.
+
+    :param soup: The BeautifulSoup object to extract the title from.
+    :param selector: The CSS selector to use for selecting the title.
+    :return: The text of the title if found, otherwise None.
     """
-    return "Hello World!"
+    if not isinstance(soup, BeautifulSoup):
+        return None
+    title = soup.select_one(selector)
+    return title.text.strip() if title and title.text else None
+
+
+def get_paragraphs(soup: BeautifulSoup, selector: str):
+    if not isinstance(soup, BeautifulSoup):
+        return None
+    paragraph = soup.select_one(selector)
+    # print(paragraphs)
+    # paragraph = paragraph.text.strip()
+    # print(paragraph)
+    return paragraph.text.strip() if paragraph and paragraph.text else None
+
+
+def get_links(base_url: str):
+    res = requests.get(base_url)
+    soup = BeautifulSoup(res.content, "html.parser")
+    links = []
+    for link in soup.find_all("a"):
+        links.append(link.get("href"))
+    return links
---
+++ webs.py
@@ -0,0 +1,7 @@
+websites = [
+    {
+        "index": "https://www.cnnindonesia.com/",
+        "title_selector": "h1.mb-2",
+        "paragraph_selector": ".detail-text",
+    }
+]
